27d6b87860e60c2af1f94172cffbf9c0976cff3d
Mihailo Dilparić <mihailo.dilparic@gmail.com>
2025-11-02 11:46:33 +0100

    feat:  base auth schema

---
+++ client/src/schemas/AuthSchemas.ts
@@ -0,0 +1,17 @@
+/* eslint-disable camelcase */
+
+import { object, string, uuidv4 } from "zod";
+
+export const LoginSchema = object({
+  id: uuidv4(),
+  username: string(),
+  firstName: string(),
+  lastName: string(),
+  imageId: uuidv4().nullable(),
+});
+
+export const LoginSearchParamsSchema = object({
+  auth_url: string().optional(),
+  code_challenge: string().optional(),
+  state: string().optional(),
+});

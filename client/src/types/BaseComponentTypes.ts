f526da41735fea215aaf03da636862f00392f063
Mihailo Dilparić <mihailo.dilparic@gmail.com>
2025-11-02 11:57:29 +0100

    feat: base component type

---
+++ client/src/types/BaseComponentTypes.ts
@@ -0,0 +1,41 @@
+import type { MouseEventHandler, ReactNode } from "react";
+
+import type { AvailableIcons } from "../enums";
+
+export type Variant =
+  | "primary"
+  | "secondary"
+  | "info"
+  | "success"
+  | "warning"
+  | "error";
+export type BaseFormComponent = {
+  title?: string;
+  variant?: Variant;
+};
+
+export type BaseFormEntryComponent<T = string | number> = {
+  name: string;
+  onChange: (e: { name: string; value: T }) => void;
+  isDisabled?: boolean;
+  value: T | null;
+  helperText?: string;
+  errors?: ({ message: string } | undefined)[] | undefined | null;
+};
+
+export type BaseComponentActionType = {
+  id: string;
+  title?: string;
+  variant?: Variant;
+  // size?: Size;
+  isDisabled?: boolean;
+  icon?: AvailableIcons;
+  iconColor?: string;
+  // iconThickness?: IconThickness;
+  className?: string;
+  onClick: MouseEventHandler<HTMLButtonElement> | undefined;
+  // items?: DropdownItemType[];
+  // allowedPlacements?: Placement[];
+  tooltip?: string | ReactNode;
+  tooltipDelay?: { open?: number; close?: number } | number;
+};
--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,3 +1,4 @@
 export * from "./BaseAuthTypes";
+export * from "./BaseComponentTypes";
 export * from "./BaseRequestTypes";
 export * from "./BaseResponseTypes";

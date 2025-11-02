af656f516192e9d5666fc14323113bad8690cd03
Mihailo Dilparić <mihailo.dilparic@gmail.com>
2025-11-02 11:57:21 +0100

    feat: icon enum

---
+++ client/src/enums/IconEnums.ts
@@ -0,0 +1,16 @@
+export const Icons = {
+  add: "ph:plus" as const,
+  arrowDown: "lucide:chevron-down" as const,
+  arrowLeft: "lucide:chevron-left" as const,
+  arrowRight: "lucide:chevron-right" as const,
+  arrowUp: "lucide:chevron-up" as const,
+  dashboard: "clarity:dashboard-line" as const,
+  expand: "ci:expand" as const,
+  notification: "ph:bell" as const,
+  search: "tabler:search" as const,
+  tasks: "material-symbols:task-alt-rounded" as const,
+  upload: "tabler:upload" as const,
+  user: "lucide:user" as const,
+};
+
+export type AvailableIcons = (typeof Icons)[keyof typeof Icons];
--- client/src/enums/index.ts
+++ client/src/enums/index.ts
@@ -1,1 +1,2 @@
+export * from "./IconEnums";
 export * from "./ModelEnums";

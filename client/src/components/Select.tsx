d29498f323ec7a1714842961068b1fb3b193b175
Mihailo Dilparić <mihailo.dilparic@gmail.com>
2025-11-02 16:54:41 +0100

    style: adjust select styles

--- client/src/components/Select.tsx
+++ client/src/components/Select.tsx
@@ -28,32 +28,39 @@ const classes = tv({
     base: "flex h-10 max-h-10 min-h-10 w-full flex-1 cursor-pointer items-center justify-between gap-x-2 rounded-lg border bg-white px-2 outline-1 outline-transparent",
     labelClasses: "max-h-4 text-sm",
     container: "group flex flex-col gap-y-1",
+    optionsContainer: "rounded-md border bg-white shadow outline-0",
   },
   variants: {
     variant: {
       primary: {
         base: "border-primary-highlight focus-within:outline-info-highlight",
         labelClasses: "text-primary-darkened group-focus-within:text-zinc-600",
+        optionsContainer: "border-primary",
       },
       secondary: {
         base: "border-secondary focus-within:outline-info-highlight",
         labelClasses: "text-secondary group-focus-within:text-primary",
+        optionsContainer: "border-secondary",
       },
       info: {
         base: "border-info focus-within:outline-info-highlight",
         labelClasses: "text-info group-focus-within:text-info-highlight",
+        optionsContainer: "border-info",
       },
       success: {
         base: "border-success focus-within:outline-success-highlight",
         labelClasses: "text-success group-focus-within:text-success-highlight",
+        optionsContainer: "border-success",
       },
       warning: {
         base: "border-warning focus-within:outline-warning-highlight",
         labelClasses: "text-warning group-focus-within:text-warning-highlight",
+        optionsContainer: "border-warning",
       },
       error: {
         base: "border-error focus-within:outline-error-highlight",
         labelClasses: "text-error group-focus-within:text-error-highlight",
+        optionsContainer: "border-error",
       },
     },
   },
@@ -107,7 +114,9 @@ export function Select({
     [dismiss, role, listNav, click]
   );
 
-  const { base, labelClasses, container } = classes({ variant });
+  const { base, labelClasses, container, optionsContainer } = classes({
+    variant,
+  });
 
   const selectedLabel =
     options.find((option) => option.value === value)?.label || null;
@@ -130,7 +139,7 @@ export function Select({
           <FloatingFocusManager context={context} modal={false}>
             <div
               ref={refs.setFloating}
-              className="border-primary rounded-xl border bg-white shadow outline-0"
+              className={optionsContainer()}
               style={{
                 ...floatingStyles,
                 overflowY: "auto",
@@ -145,7 +154,7 @@ export function Select({
                     listRef.current[i] = node;
                   }}
                   role="option"
-                  className={`border-t-secondary cursor-pointer overflow-hidden border-t p-2 outline-0 transition-colors first:rounded-t-xl first:border-t-0 last:rounded-b-xl ${i === activeIndex || item?.value === value ? "bg-info-highlight/20 text-info" : "text-black"}`}
+                  className={`border-t-secondary cursor-pointer overflow-hidden border-t p-2 outline-0 transition-colors first:rounded-t-md first:border-t-0 last:rounded-b-md ${i === activeIndex || item?.value === value ? "bg-info-highlight/20 text-info" : "text-black"}`}
                   tabIndex={i === activeIndex ? 0 : -1}
                   aria-selected={value === item?.value && i === activeIndex}
                   {...getItemProps({

891f8817693777018145a067c68e4c7055f0a7b8
Mihailo Dilparić <mihailo.dilparic@gmail.com>
2025-11-02 16:55:43 +0100

    feat: select for form config

--- client/src/utils/form.ts
+++ client/src/utils/form.ts
@@ -1,10 +1,11 @@
 import { createFormHook, createFormHookContexts } from "@tanstack/react-form";
 
-import { Button, Input } from "../components";
+import { Button, Input, Select } from "../components";
 const { fieldContext, formContext } = createFormHookContexts();
 export const { useAppForm } = createFormHook({
   fieldComponents: {
     Input,
+    Select,
   },
   formComponents: {
     Button,

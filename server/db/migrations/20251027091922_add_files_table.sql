82b8ea80d6f6942cf249b4ebec2a81f4cc7ec05f
Mihailo Dilparić <mihailo.dilparic@gmail.com>
2025-11-02 17:25:51 +0100

    chore: change image table to files table

--- server/db/migrations/20251027091922_add_files_table.sql
+++ server/db/migrations/20251027091922_add_files_table.sql
@@ -1,11 +1,13 @@
 -- migrate:up
 CREATE TABLE IF NOT EXISTS
-    images (
+    files (
         id UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid (),
         title TEXT NOT NULL,
         created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-        owner_id UUID REFERENCES users (id) ON DELETE CASCADE
+        owner_id UUID REFERENCES users (id) ON DELETE CASCADE,
+        type TEXT NOT NULL,
+        size FLOAT8 NOT NULL DEFAULT 0
     );
 
 -- migrate:down
-DROP TABLE IF EXISTS images;
+DROP TABLE IF EXISTS files;

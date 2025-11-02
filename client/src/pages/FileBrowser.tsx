039dca56d37fd8e5e3ccda9eb17204eb89ef1d2a
Mihailo Dilparić <mihailo.dilparic@gmail.com>
2025-11-02 17:08:36 +0100

    feat: sort filter and view icons

--- client/src/pages/FileBrowser.tsx
+++ client/src/pages/FileBrowser.tsx
@@ -1,18 +1,26 @@
 import { createLazyRoute } from "@tanstack/react-router";
 import { useState } from "react";
 
-import { Button, FileCard, Input } from "../components";
+import { Button, FileCard, Input, Select } from "../components";
 import { Icons } from "../enums";
+
+const sortOptions = [
+  { id: "size", label: "Size", value: "size" },
+  { id: "title", label: "Title", value: "title" },
+  { id: "type", label: "Type", value: "type" },
+  { id: "created_at", label: "Uploaded at", value: "created_at" },
+];
 
 function FileBrowser() {
   const [files, setFiles] = useState<FileList>();
+  const [view, setView] = useState<"grid" | "list">("grid");
 
   return (
     <div className="w-full mx-auto h-full flex flex-col gap-y-4">
       <div className="w-full h-14 flex items-center justify-center">
         <h1 className="text-3xl font-bold">Filestore</h1>
       </div>
-      <div className="w-full px-6 flex flex-col gap-y-8 mx-auto flex-1">
+      <div className="w-full px-6 flex flex-col gap-y-10 mx-auto flex-1">
         <div className="rounded-md border border-secondary w-full p-4 flex items-center flex-nowrap gap-x-4">
           <div className="h-10 grow">
             <Input
@@ -39,8 +47,40 @@ function FileBrowser() {
           </div>
         </div>
         <hr className="border-secondary" />
-        <h2 className="text-2xl font-semibold">Your Files</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+        <div className="flex items-center justify-between">
+          <h2 className="text-2xl font-semibold">Your Files</h2>
+          <div className="flex items-center gap-x-2">
+            <div className="w-46">
+              <Select
+                options={sortOptions}
+                onChange={() => {}}
+                name="sort"
+                value=""
+                variant="secondary"
+              />
+            </div>
+
+            <Button
+              isOutline
+              icon={Icons.sort}
+              onClick={undefined}
+              variant="secondary"
+            />
+            <Button
+              isOutline={view !== "grid"}
+              icon={Icons.grid}
+              variant={view === "grid" ? "info" : "secondary"}
+              onClick={() => setView("grid")}
+            />
+            <Button
+              icon={Icons.list}
+              isOutline={view !== "list"}
+              variant={view === "list" ? "info" : "secondary"}
+              onClick={() => setView("list")}
+            />
+          </div>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
           <FileCard />
         </div>
       </div>

b80ddd50b8aa21c242d2ec5e4825b1b9b53acc6e
Mihailo Dilparić <mihailo.dilparic@gmail.com>
2025-11-02 12:41:51 +0100

    fix: add missing peer dep

---
+++ client/src/router.tsx
@@ -0,0 +1,117 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  createRootRoute,
+  createRoute,
+  createRouter,
+  Outlet,
+  redirect,
+} from "@tanstack/react-router";
+import { zodValidator } from "@tanstack/zod-adapter";
+
+import { MainLayout } from "./components";
+import { LoginSearchParamsSchema } from "./schemas";
+import type { BaseAuthCallbackType } from "./types";
+import { authFetchFunction } from "./utils";
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: 3 * 60 * 1000, // expressed in ms, equal to 3 mins
+    },
+  },
+});
+
+const rootRoute = createRootRoute({
+  beforeLoad: async () => {
+    return {
+      auth: await queryClient.ensureQueryData<
+        BaseAuthCallbackType | { user: null }
+      >({
+        queryKey: ["user"],
+        queryFn: async () => {
+          try {
+            const res = await authFetchFunction<BaseAuthCallbackType>({
+              method: "GET",
+              url: "session",
+            });
+            if (res.ok && res.data) return res.data;
+            else return { user: null };
+          } catch (error) {
+            console.error(error);
+            return { user: null };
+          }
+        },
+        staleTime: 8 * 60 * 60 * 1000,
+      }),
+    };
+  },
+  loader: (ctx) => {
+    if (
+      !ctx.context.auth.user &&
+      !ctx.location.pathname.endsWith("/auth/login") &&
+      !ctx.location.pathname.endsWith("/auth/register")
+    )
+      throw redirect({ to: "/auth/login" });
+    if (
+      !!ctx.context.auth.user?.id &&
+      ctx.location.pathname.endsWith("/auth/login")
+    )
+      throw redirect({ to: "/" });
+  },
+  component: () => <Outlet />,
+});
+const indexRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: "/",
+  component: () => (
+    <QueryClientProvider client={queryClient}>
+      <MainLayout>
+        <Outlet />
+      </MainLayout>
+    </QueryClientProvider>
+  ),
+});
+//#region Auth Routes
+const authRoute = createRoute({
+  beforeLoad: (ctx) => {
+    if (ctx.location.pathname === "/auth")
+      throw redirect({ to: "/auth/login" });
+  },
+  getParentRoute: () => rootRoute,
+  path: "/auth",
+  component: Outlet,
+});
+
+const registerRoute = createRoute({
+  getParentRoute: () => authRoute,
+  path: "/register",
+}).lazy(() => import("./pages/routes").then((d) => d.registerLazyRoute));
+
+const loginRoute = createRoute({
+  getParentRoute: () => authRoute,
+  validateSearch: zodValidator(LoginSearchParamsSchema),
+  path: "/login",
+  beforeLoad: async (ctx) => {
+    if (!ctx.search.code_challenge || !ctx.search.state) {
+      const res = await authFetchFunction<{ auth_url: string; state: string }>({
+        url: "login",
+        method: "POST",
+      });
+      if (res.ok && res.data) window.location.replace(res.data.auth_url);
+    }
+  },
+}).lazy(() => import("./pages/routes").then((d) => d.loginLazyRoute));
+
+const routeTree = rootRoute.addChildren([
+  indexRoute,
+  authRoute.addChildren([registerRoute, loginRoute]),
+]);
+
+export const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}

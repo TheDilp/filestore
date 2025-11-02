f42f19646bbd96ff42a9a3eed35f2f498ec1200b
Mihailo Dilparić <mihailo.dilparic@gmail.com>
2025-11-02 16:56:16 +0100

    style: adjust padding

--- client/src/components/MainLayout.tsx
+++ client/src/components/MainLayout.tsx
@@ -5,7 +5,7 @@ type Props = {
 };
 export function MainLayout({ children }: Props) {
   return (
-    <main className="max-w-screen flex h-screen max-h-screen w-screen flex-nowrap py-4 overflow-hidden overscroll-none">
+    <main className="flex h-screen max-h-screen mx-auto flex-nowrap lg:container overflow-hidden overscroll-none">
       {children}
     </main>
   );

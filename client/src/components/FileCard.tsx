d0889406d00025baeacc168b5220b5fca2dee333
Mihailo Dilparić <mihailo.dilparic@gmail.com>
2025-11-05 11:41:05 +0100

    feat: file size and better title shrink for menu

--- client/src/components/FileCard.tsx
+++ client/src/components/FileCard.tsx
@@ -2,6 +2,7 @@ import type { infer as zodInfer } from "zod";
 
 import { Icons } from "../enums";
 import type { FileSchema } from "../schemas";
+import { getFileSize, getIconColor } from "../utils";
 import { Button } from "./Button";
 import { Icon } from "./Icon";
 
@@ -14,10 +15,10 @@ export function FileCard({ title, type, createdAt, size }: Props) {
   return (
     <div className="border border-secondary p-4 rounded-md hover:shadow transition-shadow group">
       <div className="flex flex-row items-center justify-between h-10">
-        <div className="flex flex-1 flex-no-wrap max-lg:max-w-5/6 justify-between w-full items-center gap-x-4">
-          <h3 className="text-sm font-medium truncate  w-full">{title}</h3>
+        <div className="flex flex-1 flex-no-wrap justify-between max-w-full items-center gap-x-4">
+          <h3 className="text-sm font-medium line-clamp-1">{title}</h3>
           <div>
-            <Icon icon={Icons[type]} fontSize={32} />
+            <Icon icon={Icons[type]} color={getIconColor(type)} fontSize={32} />
           </div>
         </div>
         <div className="group-hover:w-8 group-hover:opacity-100 max-lg:opacity-100 max-lg:w-8 pointer-events-auto opacity-0 w-0 transition-(--fade-in-transition) duration-100">
@@ -25,7 +26,7 @@ export function FileCard({ title, type, createdAt, size }: Props) {
         </div>
       </div>
       <div className="text-xs flex flex-col gap-y-0.5 text-zinc-400">
-        <span>Size: {size} KB</span>
+        <span>Size: {getFileSize(size)}</span>
         <span>Uploaded: {createdAt}</span>
       </div>
     </div>

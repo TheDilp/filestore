32bc3ce420977cd92dd13a81c1d2b13ddd5e4b49
Mihailo Dilparić <mihailo.dilparic@gmail.com>
2025-11-05 12:08:47 +0100

    chore: remove redundant fn arg

--- client/src/utils/datetime.ts
+++ client/src/utils/datetime.ts
@@ -1,12 +1,11 @@
 import { format as formatFn, parseISO } from "date-fns";
-export function formatDateTime(
-  value: string,
-  format: string = import.meta.env.VITE_DEFAULT_DATE_TIME_FORMAT ||
-    "dd/MM/yyyy HH:mm"
-) {
+export function formatDateTime(value: string) {
   try {
     const parsed = parseISO(value.replace(/\[.*\]$/, ""));
-    return formatFn(parsed, format);
+    return formatFn(
+      parsed,
+      import.meta.env.VITE_DEFAULT_DATE_TIME_FORMAT || "dd/MM/yyyy HH:mm"
+    );
   } catch (error) {
     console.error(error);
     return "";

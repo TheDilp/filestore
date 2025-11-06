815c7db55e55f3d7d3618f4466bd289c481591cd
Mihailo Dilparić <mihailo.dilparic@gmail.com>
2025-11-06 12:32:11 +0100

    feat: notifications

--- client/src/components/MainLayout.tsx
+++ client/src/components/MainLayout.tsx
@@ -1,11 +1,27 @@
 import type { ReactNode } from "react";
 
+import { useNotifications } from "../hooks";
+import { Notification } from "./Notification";
+
 type Props = {
   children: ReactNode;
 };
 export function MainLayout({ children }: Props) {
+  const notifications = useNotifications();
   return (
     <main className="flex h-screen max-h-screen mx-auto flex-nowrap lg:container overflow-hidden overscroll-none">
+      <div className="absolute top-4 right-4 flex flex-col gap-y-2">
+        {notifications.map((notification) => (
+          <Notification
+            key={notification.id}
+            icon={notification.icon}
+            id={notification.id}
+            timer={notification.timer}
+            title={notification.title}
+            variant={notification.variant}
+          />
+        ))}
+      </div>
       {children}
     </main>
   );

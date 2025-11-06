24de3dc1dd078852fc03849e5cb9e33cc16fbbea
Mihailo Dilparić <mihailo.dilparic@gmail.com>
2025-11-06 13:30:39 +0100

    feat: is video or is text util

--- client/src/utils/files.ts
+++ client/src/utils/files.ts
@@ -2,10 +2,21 @@ const imageTypes = new Set(["png", "jpg", "jpeg", "webp", "gif", "svg"]);
 
 const audioTypes = new Set(["mp3", "wav", "ogg", "aac", "m4p"]);
 
+const videoTypes = new Set(["mp4", "webm", "ogg", "mov", "avi"]);
+
+const textTypes = new Set(["txt", "csv", "json", "xml", "md"]);
+
 export function isImage(type: string): boolean {
   return imageTypes.has(type.toLowerCase());
 }
 
 export function isAudio(type: string): boolean {
   return audioTypes.has(type.toLowerCase());
+}
+
+export function isVideo(type: string): boolean {
+  return videoTypes.has(type.toLowerCase());
+}
+export function isText(type: string): boolean {
+  return textTypes.has(type.toLowerCase());
 }

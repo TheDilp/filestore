9fb85828c6526c1d60a7b41cf2773ef0e1ca11ea
Mihailo Dilparić <mihailo.dilparic@gmail.com>
2025-11-29 10:49:29 +0100

    feat: actions for file rows

--- client/src/components/FileRow.tsx
+++ client/src/components/FileRow.tsx
@@ -1,19 +1,28 @@
+import { useQueryClient } from "@tanstack/react-query";
 import { Link, useParams } from "@tanstack/react-router";
 import type { infer as zodInfer } from "zod";
 
 import { Icons } from "../enums";
 import type { FileSchema } from "../schemas";
-import { formatDateTime, getFileSize, getIconColor } from "../utils";
+import {
+  fetchFunction,
+  fileFetchFunction,
+  formatDateTime,
+  getFileSize,
+  getIconColor,
+} from "../utils";
 import { Button } from "./Button";
+import { Dropdown } from "./Dropdown";
 import { Icon } from "./Icon";
 
 type Props = Pick<
   zodInfer<typeof FileSchema>,
-  "title" | "type" | "createdAt" | "size"
+  "id" | "title" | "type" | "createdAt" | "size"
 >;
 
-export function FileRow({ title, type, createdAt, size }: Props) {
+export function FileRow({ id, title, type, createdAt, size }: Props) {
   const params = useParams({ from: "/browser/{-$path}" });
+  const queryClient = useQueryClient();
 
   return (
     <Link
@@ -31,12 +40,58 @@ export function FileRow({ title, type, createdAt, size }: Props) {
             <span>{getFileSize(size)}</span>
             <span className="text-xs">{formatDateTime(createdAt)}</span>
             <div className="group-hover:w-8 group-hover:opacity-100 max-lg:opacity-100 max-lg:w-8 pointer-events-auto opacity-0 w-0 transition-(--fade-in-transition) duration-100">
-              <Button
-                onClick={undefined}
-                hasNoBorder
-                isOutline
-                icon={Icons.menu}
-              />
+              <Dropdown
+                allowedPlacements={["left", "right"]}
+                items={[
+                  {
+                    id: "download",
+                    title: "Download",
+                    isHidden: type === "folder",
+                    icon: Icons.download,
+                    onClick: async () => {
+                      const blob = await fileFetchFunction({
+                        id,
+                        searchParams: [["path", params.path || ""]],
+                      });
+                      const link = document.createElement("a");
+                      link.href = URL.createObjectURL(blob);
+                      link.download = title;
+                      link.click();
+                      URL.revokeObjectURL(link.href);
+                      link.remove();
+                    },
+                  },
+                  {
+                    id: "share",
+                    title: "Share",
+                    isHidden: type === "folder",
+                    icon: Icons.share,
+                    onClick: undefined,
+                  },
+                  {
+                    id: "delete",
+                    title: "Delete",
+                    icon: Icons.delete,
+                    onClick: async () => {
+                      await fetchFunction({
+                        model: "files",
+                        action: "delete",
+                        method: "DELETE",
+                        id,
+                      });
+                      queryClient.invalidateQueries({ queryKey: ["files"] });
+                    },
+                    iconColor: "red",
+                  },
+                ]}
+              >
+                <Button
+                  onClick={(e) => e.preventDefault()}
+                  hasNoBorder
+                  isOutline
+                  icon={Icons.menu}
+                />
+              </Dropdown>
             </div>
           </span>
         </div>

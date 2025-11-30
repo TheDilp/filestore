549ffdba42da480ac5fcc243a6881087f74b889d
Mihailo Dilparić <mihailo.dilparic@gmail.com>
2025-11-30 09:26:11 +0100

    feat: save darkmode to ls

--- client/src/hooks/useDarkMode.ts
+++ client/src/hooks/useDarkMode.ts
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 
 export function useDarkMode() {
-  const [mode, setMode] = useState("light");
+  const [mode, setMode] = useState(localStorage.getItem("mode") || "light");
 
   useEffect(() => {
     if (mode === "dark") document.body.classList.add("dark");
     else document.body.classList.remove("dark");
+    localStorage.setItem("mode", mode);
   }, [mode]);
 
   return {

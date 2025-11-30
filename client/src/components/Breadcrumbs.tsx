d9bfe3d081ca85029c5e693092ad40ad76eccf75
Mihailo Dilparić <mihailo.dilparic@gmail.com>
2025-11-30 09:25:54 +0100

    fix: breadcrumb darkmode styling

--- client/src/components/Breadcrumbs.tsx
+++ client/src/components/Breadcrumbs.tsx
@@ -7,18 +7,20 @@ type Props = {
   items: { id: string; title: string; path: string }[];
 };
 export function Breadcrumbs({ items = [] }: Props) {
-
-
   return (
     <nav className="w-full text-xl max-h-fit">
       <ul className="w-full h-full flex flex-row gap-x-1">
         <li className="text-primary items-center flex gap-x-1">
           <Link
-            className="text-primary items-center flex gap-x-1"
+            className="text-primary items-center flex gap-x-1 dark:text-white"
             to="/browser/{-$path}"
             params={{ path: "" }}
           >
-            <span className={`${items.length > 0 ? "text-info-highlight" : ""}`}>Home</span>
+            <span
+              className={`${items.length > 0 ? "text-info-highlight" : ""}`}
+            >
+              Home
+            </span>
             {items.length > 0 ? (
               <span>
                 <Icon icon={Icons.arrowRight} />
@@ -27,10 +29,7 @@ export function Breadcrumbs({ items = [] }: Props) {
           </Link>
         </li>
         {items.map((item, idx) => (
-          <li
-
-            key={item.id}
-          >
+          <li key={item.id} className="">
             <Link
               className="flex items-center gap-x-1"
               to="/browser/{-$path}"
@@ -41,7 +40,11 @@ export function Breadcrumbs({ items = [] }: Props) {
                   .join("/"),
               }}
             >
-              <span className={`${idx < items.length - 1 ? "text-info-highlight" : "text-primary"}`}>{item.title}</span>
+              <span
+                className={`${idx < items.length - 1 ? "text-info-highlight" : "text-primary dark:text-white"}`}
+              >
+                {item.title}
+              </span>
               {idx < items.length - 1 ? (
                 <span>
                   <Icon icon={Icons.arrowRight} />

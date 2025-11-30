17769ebf08ded57d5c49766b51c9df06d1184c07
Mihailo Dilparić <mihailo.dilparic@gmail.com>
2025-11-30 14:32:02 +0100

    chore: use path within useupload hook

--- client/src/hooks/useUpload.ts
+++ client/src/hooks/useUpload.ts
@@ -1,17 +1,22 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, type UseMutationOptions } from "@tanstack/react-query";
+import { useParams } from "@tanstack/react-router";
 
+import type { FileForUpload } from "../types";
 import { fetchFunction } from "../utils";
 
-async function uploadFiles({
-  files,
-  path,
-}: {
-  files: { file: File; name: string }[];
-  path: string | undefined;
-}) {
+async function uploadFiles(
+  {
+    files,
+  }: {
+    files: FileForUpload[];
+  },
+  ctx: {
+    meta: UseMutationOptions["meta"];
+  }
+) {
   if (!files) return;
   const formData = new FormData();
-
+  const path = ctx?.meta?.path as string | undefined;
   for (let index = 0; index < files.length; index++)
     formData.append(`file${index}`, files[index].file, files[index].name);
 
@@ -30,5 +35,11 @@ async function uploadFiles({
 }
 
 export function useUpload() {
-  return useMutation({ mutationFn: uploadFiles });
+  const params = useParams({ from: "/browser/{-$path}" });
+
+  return useMutation({
+    mutationKey: ["upload"],
+    mutationFn: uploadFiles,
+    meta: { path: params.path },
+  });
 }
